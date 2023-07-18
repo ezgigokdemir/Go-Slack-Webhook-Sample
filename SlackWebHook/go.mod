08b7231a22d248a53868c3887b9264ece8121349
Ezgi Gökdemir <38529111+ezgigokdemir@users.noreply.github.com>
2023-07-18 16:38:15 +0300

    all files uploaded

---
+++ SlackWebHook/go.mod
@@ -0,0 +1,17 @@
+module webHookApp
+
+go 1.20
+
+require (
+	github.com/ashwanthkumar/slack-go-webhook v0.0.0-20200209025033-430dd4e66960
+	github.com/google/uuid v1.3.0
+	github.com/parnurzeal/gorequest v0.2.16
+)
+
+require (
+	github.com/elazarl/goproxy v0.0.0-20221015165544-a0805db90819 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/smartystreets/goconvey v1.8.1 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	moul.io/http2curl v1.0.0 // indirect
+)
---
+++ SlackWebHook/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/parnurzeal/gorequest"
+)
+
+type Message struct {
+	ID      string
+	Subject string `json:"subject"`
+	Content string `json:"content"`
+}
+
+type Payload struct {
+	Username  string `json:"username,omitempty"`
+	IconEmoji string `json:"icon_emoji,omitempty"`
+	Channel   string `json:"channel,omitempty"`
+	Text      string `json:"text,omitempty"`
+}
+
+func Send(webhookUrl string, proxy string, payload Payload) []error {
+	resp, _, err := gorequest.New().
+		Proxy(proxy).
+		Post(webhookUrl).
+		Send(payload).
+		End()
+
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode >= 400 {
+		return []error{fmt.Errorf("Error for sending a message. Status: %v", resp.Status)}
+	}
+
+	return nil
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+
+	// Parse the request body to get the message data
+	var m Message
+	err := json.NewDecoder(r.Body).Decode(&m)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Failed to parse request body: %v", err)
+		return
+	}
+
+	// Set an unique id for message
+	guid := uuid.New()
+	m.ID = guid.String()
+
+	// Send message to slack
+	webhookUrl := "your web hook url"
+
+	payload := Payload{
+		Text:      fmt.Sprintf("ID: %s\nSubject: %s\nContent: %s", m.ID, m.Subject, m.Content),
+		Username:  "your username",
+		Channel:   "#logs",
+		IconEmoji: ":monkey_face:",
+	}
+
+	errs := Send(webhookUrl, "", payload)
+	if len(errs) > 0 {
+		for _, err := range errs {
+			fmt.Printf("error: %s\n", err)
+		}
+	}
+
+	// Return the created model as a response
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(m)
+}
+
+func main() {
+	// Routes and handlers
+	http.HandleFunc("/tasks", CreateTask)
+
+	// Start the server
+	log.Println("The server is listening on port 8000...")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("Server error: ", err)
+	}
+}
